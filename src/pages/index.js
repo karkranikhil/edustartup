1cd2a3bb65fc4f53628106962a7bff0348d5014e
Nikhil Karkra <karkra.nikhil@gmail.com>
2021-07-26 20:41:28 +1000

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ export default function IndexPage() {
     <ThemeProvider theme={theme}>
       <Layout>
         <SEO
-          title="Startup agency landing template"
-          description="Collection of free top of the line startup landing templates built using react/ next js. Free to download, simply edit and deploy! Updated weekly!"
+          title="Salesforce training institute"
+          description=""
         />
         <Banner />
         <OtherServices />
